ab721e5cba7ba6b958e4c4a27a7c49b4f0e0b832
Ivasenko <gafrik2000@mail.ru>
2024-05-20 14:21:48 +0300

    init

---
+++ src/main/java/effective/mobile/BankOperationsService/BankOperationsServiceApplication.java
@@ -0,0 +1,13 @@
+package effective.mobile.BankOperationsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOperationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOperationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/effective/mobile/BankOperationsService/BankOperationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package effective.mobile.BankOperationsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOperationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
